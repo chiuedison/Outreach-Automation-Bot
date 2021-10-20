162c59ec5eaccd85bd6f92da9755310b24cfd9d6
Pasha Khomchenko <32812310+pashakhomchenko@users.noreply.github.com>
2021-10-20 17:51:14 -0400

    Added .env file for tokens and api keys, requirements.txt for required packages, .gitignore and a method for updating Airtable

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import requests
+from dotenv import load_dotenv
+from pyairtable import Table
 
 
 class BearerAuth(requests.auth.AuthBase):
@@ -18,10 +21,10 @@ class Connector:
         '''Currently doesn't initialize any variables'''
         pass
 
-    def getTiltifyData(self, token):
+    def getTiltifyData(self):
         '''Currently this method calls Tiltify API and prints out username of authenticated user'''
         url_user = 'https://tiltify.com/api/v3/user'
-        r = requests.get(url_user, auth=BearerAuth(token))
+        r = requests.get(url_user, auth=BearerAuth(os.getenv('TILTIFY_TOKEN')))
         if r.status_code == requests.codes.ok:
             print('Succesful HTTP request to Tiltify API')
         else:
@@ -30,8 +33,18 @@ class Connector:
         response_content = r.json()
         print(response_content['data']['username'])
 
+    def updateAirtable(self, base_id, table_name):
+        '''Currently this method creates a record in the table, updates it and deletes it'''
+        table = Table(os.getenv('AIRTABLE_API_KEY'), base_id, table_name)
+        id = table.create({'username': 'edisonc', 'donation': 1000})['id']
+        print(table.get(id))
+        table.update(id, {'donation': 5000})
+        print(table.get(id))
+        table.delete(id)
+
 
 if __name__ == "__main__":
-    tiltify_token = 'c057949a35b983ff9c4ff4b79bd5a66dad04bb762825ef8533bca7f25ce1efcd'
+    load_dotenv()
     c = Connector()
-    c.getTiltifyData(tiltify_token)
+    c.getTiltifyData()
+    c.updateAirtable(base_id='appySrjTEBJI6l353', table_name='Table 1')
