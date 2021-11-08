3c8b5b57ceb26b5f79e657c7d84a5cce144a8732
Pasha Khomchenko <32812310+pashakhomchenko@users.noreply.github.com>
2021-11-08 17:10:33 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -2,19 +2,27 @@ import os
 from dotenv import load_dotenv
 import tweepy
 
-# Load environment variables
-load_dotenv()
 
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler(
-    os.getenv('TWITTER_API_KEY'), os.getenv('TWITTER_API_KEY_SECRET'))
-auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'),
-                      os.getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+def create_api():
+    ''' Creates and authenticates Twitter API'''
+    # Load environment variables
+    load_dotenv()
+    # Twitter API credentials
+    auth = tweepy.OAuthHandler(
+        os.getenv('TWITTER_API_KEY'), os.getenv('TWITTER_API_KEY_SECRET'))
+    auth.set_access_token(os.getenv('TWITTER_ACCESS_TOKEN'),
+                          os.getenv('TWITTER_ACCESS_TOKEN_SECRET'))
+    return tweepy.API(auth)
 
-api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+def send_dms(api):
+    ''' Sends a direct message to the user '''
+    # Get the user's ID
+    id = api.get_user(screen_name='@ninja').id
+    # Send the message
+    api.send_direct_message(id, 'Hello!')
+
+
+if __name__ == "__main__":
+    api = create_api()
+    send_dms(api)
